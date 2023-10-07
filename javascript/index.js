9e2e64a3c3cb0e04383beffa0510a04709e386dc
Eneias Raul <eneias.raul@academico.ifpb.edu.br>
2023-10-07 10:53:29 -0300

    Gerando opção de login de um usuário já registrado

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+// seu-arquivo.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Oculta a tela de carregamento após o carregamento da página
+    document.getElementById("loading_screen").style.display = "none";
+});
