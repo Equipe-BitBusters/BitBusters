96ee0d760d6913c116167fbea9a1b07cd99cf287
César <cesar.caju@academico.ifpb.edu.br>
2023-10-12 18:06:39 -0300

    organização das páginas

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Adiciona um atraso de 2 segundos (2000 milissegundos) antes de ocultar o elemento de carregamento
     setTimeout(function() {
         document.getElementById('loading_screen').style.display = 'none';
     }, 2000);
-
-    // Adiciona um ouvinte de clique ao botão de login para ocultar o elemento de carregamento
     document.getElementById('login_button').addEventListener('click', function() {
         document.getElementById('loading_screen').style.display = 'none';
-        // Se você deseja redirecionar após ocultar o elemento de carregamento, descomente a linha abaixo
-        // window.location.href = "/Pagina_Secretarias/Pagina_Secretarias.html";
     });
 });
